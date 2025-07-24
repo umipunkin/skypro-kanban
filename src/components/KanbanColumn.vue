d579ced89ecd7a657c9f263bc1c3d41ba4d4e69d
umipunkin <teddmandarinov@gmail.com>
2025-07-24 20:07:14 +0300

    Выполнил Первое дз

---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TaskCard from '@/components/TaskCard.vue'
+
+defineProps({
+  title: String
+})
+
+
+</script>
+
+<template>
+  <div class="main__column column">
+    <div class="column__title">
+      <p>{{ title }}</p>
+    </div>
+    <div class="cards">
+      <TaskCard v-for="i in 3" :key="i" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
